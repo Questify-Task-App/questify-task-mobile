f2cfa1aee0f2feec610032abbb240a53ea4f9ee7
Caique Carvalho da Silva <71516065+Caiqueoak@users.noreply.github.com>
2025-07-28 10:28:22 -0300

    Update coin_banner.dart

--- lib/widgets/coin_banner.dart
+++ lib/widgets/coin_banner.dart
@@ -16,8 +16,8 @@ class _CoinBannerState extends State<CoinBanner> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color: Colors.red,
-      color: Colors.blueAccent,
+      color: Colors.red,
+      // color: Colors.blueAccent,
       width: double.infinity,
       padding: EdgeInsets.all(30),
       child: Column(
